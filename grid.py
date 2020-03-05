bf8a9ea2c520282e0f475428c49c6047ffb3c977
nicole894 <nicolegonsalves894@gmail.com>
2020-03-05 14:17:42 -0500

    Testing the travis.yml file

---
+++ grid.py
@@ -0,0 +1,26 @@
+def grid(dimensions, x, y, path):
+
+    for direction in path:
+        if direction == 'R':
+            if y-1 < 0:
+                y = dimensions-1
+            else:
+                y -= 1
+
+        if direction == 'L':
+            if y+1 == dimensions:
+                y = 0
+            else:
+                y += 1
+
+        if direction == 'U':
+            if x-1 >= 0:
+                x -= 1
+
+        if direction == 'D':
+            if x + 1 < dimensions:
+                x += 1
+
+    final_position = [x, y]
+
+    return final_position
---
+++ hackerRankTest.py
@@ -0,0 +1,33 @@
+import unittest
+from grid import grid
+
+class Test(unittest.TestCase):
+
+    def test_grid(self):
+        expected_id = [0,2];
+        generated_id = grid(3,0,0,"RR")
+        self.assertEqual(expected_id, generated_id)
+        
+
+if __name__ == '__main__':
+    unittest.main()
+
+        
+
+        
+   
+
+
+
+
+
+        
+
+        
+   
+
+
+
+
+
+
